a430653608dbaba4d68e460a8449644d52eb0ea8
Tim Gangl <tim.gangl30@gmail.com>
2019-10-27 19:23:36 -0700

    html done link for database in

---
+++ assets/train.js
@@ -0,0 +1,27 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAM8XDx5uQiSP4Y3XiHqUeEWpWunA6ZIWU",
+  authDomain: "train-scheduler-98c9b.firebaseapp.com",
+  databaseURL: "https://train-scheduler-98c9b.firebaseio.com",
+  projectId: "train-scheduler-98c9b",
+  storageBucket: "train-scheduler-98c9b.appspot.com",
+  messagingSenderId: "336698376932",
+  appId: "1:336698376932:web:ce251e6bc4dbed46b6e2f1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//create function to add info to variable
+
+//create div to push var info into
+
+//push info from variable to div in schedule
+
+//send to html
+
+//create moment code for frequency
+
+// function to calc next train time
+
+//compare train time to current time
+
+//send data to database
